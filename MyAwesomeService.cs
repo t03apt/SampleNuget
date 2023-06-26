72503c119b6eb35410b83573da96d1b4a53d857c
Tamás Papp <tpapp@itron.com>
2023-06-26 23:50:31 +0200

    .

---
+++ MyAwesomeService.cs
@@ -0,0 +1,17 @@
+﻿namespace SampleNuget;
+
+public class MyAwesomeService
+{
+    public static void WriteFibonacci(int len)
+    {
+        int a = 0, b = 1;
+        Console.Write("{0} {1}", a, b);
+        for (var i = 2; i < len; i++)
+        {
+            var c = a + b;
+            Console.Write(" {0}", c);
+            a = b;
+            b = c;
+        }
+    }
+}
